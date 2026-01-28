972fa6f67a466c67f728c7d9ad7cecc01a8f5817
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2026-01-28 13:12:14 +0530

    Update outward-manage.php

--- admin/outward-manage.php
+++ admin/outward-manage.php
@@ -107,7 +107,7 @@ if (isset($_GET['del'])) {
                                 </abbr>
                             </span>
                             <span style="float:right;margin-left:15px;">
-                                <abbr title="Import In Excel" style="cursor: pointer" class="abbr_class"><a href="outward-data-export.php?date=<?php echo $fdate; ?>"><img src="images/excel.png" style="width:30px;height:30px;"></a></abbr>
+                                <abbr title="Import In Excel" style="cursor: pointer" class="abbr_class"><a href="outward-data-export.php?date=<?php echo $fdate; ?>">EXCEL</a></abbr>
                             </span>
                             <span style="float:right;">
 								<abbr title="Add New" style="cursor: pointer" class="abbr_class"><a href="outward-add.php"><img src="images/add.png" class="image_safe" style="width:30px;height:30px;"></a></abbr>
