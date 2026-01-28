215ff72922e00c8dc9e81463eba92e0bc0978073
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2026-01-28 13:10:48 +0530

    Update inward-manage.php

--- admin/inward-manage.php
+++ admin/inward-manage.php
@@ -100,7 +100,7 @@ if (isset($_GET['del'])) {
                                 </abbr>
                             </span>
                             <span style="float:right;margin-left:15px;">
-                                <abbr title="Import In Excel" style="cursor: pointer" class="abbr_class"><a href="inward-data-export.php?date=<?php echo $fdate; ?>"><img src="images/excel.png" style="width:30px;height:30px;"></a></abbr>
+                                <abbr title="Import In Excel" style="cursor: pointer" class="abbr_class"><a href="inward-data-export.php?date=<?php echo $fdate; ?>">EXCEL</a></abbr>
                             </span>
                             <span style="float:right;">
                                 <abbr title="Add New" style="cursor: pointer" class="abbr_class"><a href="inward-add.php"><img src="images/add.png" class="image_safe" style="width:30px;height:30px;"></a></abbr>
